39eba24965b08bb208cf097ad40ac8894ecf76ed
Charles Wang <charleswang23103@gmail.com>
2021-09-10 19:51:54 -0700

    Added new comment

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Newly Added Comment
 class App extends Component {
   constructor() {
         super()
